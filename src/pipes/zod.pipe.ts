5f99fb87f720d29353da008389a232cadf978fd6
jungai <juworaphol125@gmail.com>
2022-08-28 11:55:58 +0700

    refactor: should result data from zod & update some type

--- src/pipes/zod.pipe.ts
+++ src/pipes/zod.pipe.ts
@@ -1,16 +1,12 @@
-import { UnprocessableEntityException } from '@nestjs/common';
 import {
   PipeTransform,
   Injectable,
   ArgumentMetadata,
-  BadRequestException,
+  UnprocessableEntityException,
 } from '@nestjs/common';
 import { z } from 'zod';
 
 interface IZodPipeSchema {
-  params?: z.ZodObject<z.ZodRawShape>;
-  query?: z.ZodObject<z.ZodRawShape>;
-  body?: z.ZodObject<z.ZodRawShape>;
   params?: z.ZodObject<z.ZodRawShape, 'strict'>;
   query?: z.ZodObject<z.ZodRawShape, 'strict'>;
   body?: z.ZodObject<z.ZodRawShape, 'strict'>;
@@ -20,14 +16,15 @@ interface IZodPipeSchema {
 export class ZodValidationPipe implements PipeTransform {
   constructor(private schema: IZodPipeSchema) {}
 
-  transform(value: any, metadata: ArgumentMetadata) {
+  transform(value: unknown, metadata: ArgumentMetadata) {
     if (metadata.type === 'query' && this.schema?.query) {
       const result = this.schema.query?.safeParse(value);
+
       if (!result.success) {
         throw new UnprocessableEntityException({ msg: result.error });
       }
 
-      return value;
+      return result.data;
     }
 
     if (metadata.type === 'param' && this.schema?.params) {
@@ -36,18 +33,19 @@ export class ZodValidationPipe implements PipeTransform {
         throw new UnprocessableEntityException({ msg: result.error });
       }
 
-      return value;
+      return result.data;
     }
 
     if (metadata.type === 'body' && this.schema?.body) {
       const result = this.schema.body?.safeParse(value);
+
       if (!result.success) {
         throw new UnprocessableEntityException({ msg: result.error });
       }
 
-      return value;
+      return result.data;
     }
 
-    return value;
+    throw new UnprocessableEntityException();
   }
 }
